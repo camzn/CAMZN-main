e6e198d63813d2b05ed0d994f4b1cb6ec284f8d0
Stevehanji <truongcanhchan@gmail.com>
2024-11-24 14:46:20 +0700

    feat: I added Home and Article

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,43 @@
-export default function Home() {
-    return (
-        // <div>
-        //     <p>Hello world</p>
-        // </div>
+import Article from "./Article"
 
-        <p>HOME</p>
+export default function Home() {
+    const post = {
+        author : {
+            username : "Nguyễn Tiến Đạt",
+            status : "Online",
+            avatarURL : "../../public/images/avatar.jpg"
+        },
+        text : "Gái Đẹp :))",
+        medias : [
+            "../../public/images/img1.jpg",
+            "../../public/images/img2.jpg",
+            "../../public/images/img3.jpg",
+            "../../public/images/img4.jpg",
+            "../../public/images/img5.jpg",
+        ],
+        likes : 10,
+        isLike : false,
+        comment : []
+    }
+
+    return (
+        <div className="">
+            {/* Post */}
+            <div>
+                <Article information = {post} firstChild = {true}></Article>
+                <Article information = {post}></Article>
+                <Article information = {post}></Article>
+                <Article information = {post}></Article>
+                <Article information = {post}></Article>
+                <Article information = {post}></Article>
+                <Article information = {post}></Article>
+
+            </div>
+
+            {/* Online, Notification, post articles */}
+            <div>
+
+            </div>
+        </div>
     );
 }
