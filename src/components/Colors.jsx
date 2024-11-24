284313fc780d981fba796186a9b561e6769db66d
Stevehanji <truongcanhchan@gmail.com>
2024-11-24 08:19:36 +0700

    Fix: I think if I used object colors, but it not work when I restart reactjs, so I backup code

--- src/components/Colors.jsx
+++
@@ -1,7 +0,0 @@
-const Colors = {
-	Organe : "#FF973D",
-	Gray : "#E7E7E7",
-	DarkGray : "#B6B6B6",
-};
-
-export default Colors;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,9 @@
 export default function Home() {
-    return <div>Home</div>
+    return (
+        // <div>
+        //     <p>Hello world</p>
+        // </div>
+
+        <p>HOME</p>
+    );
 }
