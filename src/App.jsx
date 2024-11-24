703e04529ef6a660c26e42d625a8675a2beb5753
Louis <louiszn269@gmail.com>
2024-11-24 10:36:05 +0700

    feat: disable context menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 
 import Sidebar from "./components/Sidebar";
 
 function App() {
+	useEffect(() => {
+		function handleContextMenu(e) {
+			e.preventDefault();
+		}
+
+		document.addEventListener("contextmenu", handleContextMenu);
+
+		return () => {
+			document.removeEventListener("contextmenu", handleContextMenu);
+		};
+	}, []);
+
 	return (
-		<div className="flex flex-row gap-2 w-screen h-screen bg-[#F3F3F3]">
+		<div className="flex flex-row relative justify-between w-full h-screen bg-[#F3F3F3] overflow-hidden">
 			<Sidebar />
 
-			<div className="w-full h-full">
+			<div className="w-full h-full m-4 overflow-x-hidden overflow-y-auto">
 				<Outlet />
 			</div>
 		</div>
