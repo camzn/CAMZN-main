e49be1a83a29e84e9c34c6da6356a2ffa03f1083
Stevehanji <truongcanhchan@gmail.com>
2024-11-25 19:13:34 +0700

    Fix: Gird images in Artical

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -17,6 +17,50 @@ function isImage(name) {
 	return IMAGE_EXTENSIONS.some((ext) => name.endsWith(ext));
 }
 
+function roundedForImageAndVideo(index, NumberOfMedias) {
+	const paramaterRounded = "2xl";
+	switch(NumberOfMedias) {
+		case 1:
+			return `rounded-${paramaterRounded}`;
+		
+		case 2:
+			switch (index) {
+				case 0:
+					return `rounded-tl-${paramaterRounded} rounded-bl-${paramaterRounded}`;
+
+				case 1:
+					return `rounded-tr-${paramaterRounded} rounded-br-${paramaterRounded}`;
+			}
+
+		case 3:
+			switch (index) {
+				case 0:
+					return `rounded-tl-${paramaterRounded}`;
+				case 1:
+					return `rounded-tr-${paramaterRounded}`;
+				case 2:
+					return `rounded-bl-${paramaterRounded} rounded-br-${paramaterRounded}`;
+			}
+
+		default:
+			switch (index) {
+				case 0:
+					return `rounded-tl-${paramaterRounded}`;
+				case 1:
+					return `rounded-tr-${paramaterRounded}`;
+				case 2:
+					return `rounded-bl-${paramaterRounded}`;
+				case 3:
+					return `rounded-br-${paramaterRounded}`;
+			}
+	}
+}
+
+function resizeForImage(index, NumberOfMedias) {
+	if (NumberOfMedias === 1) return "w-full max-h-[600px]";
+	else if (NumberOfMedias === 3 && index === 2) return "w-full max-h-[300px]";
+}
+
 export default function Article({ post }) {
 	let [likes, setLikes] = useState(post.likes);
 	let [isLike, setIsLikes] = useState(post.isLike);
@@ -38,14 +82,14 @@ export default function Article({ post }) {
 				case 1:
 					return "grid-rows-1 grid-cols-1";
 				case 2:
-					return "grid-rows-2 grid-cols-1";
+					return "grid-rows-1 grid-cols-2";
 				default:
 					return "grid-rows-2 grid-cols-2";
 			}
 		})();
 
 	return (
-		<div className={`flex flex-col bg-[white] rounded-[10px] p-[15px] gap-3 shadow-md`}>
+		<div className={`w-[400px] flex flex-col bg-[white] rounded-[10px] p-[15px] gap-3 shadow-md`}>
 			{/* User */}
 			<div className="flex items-center gap-3">
 				<img
@@ -63,30 +107,22 @@ export default function Article({ post }) {
 					if (index >= 4) {
 						return;
 					}
-
-					const rounded = (function () {
-						switch (index) {
-							case 0:
-								return "rounded-tl-2xl";
-							case 1:
-								return "rounded-tr-2xl";
-							case 2:
-								return "rounded-bl-2xl";
-							case 3:
-								return "rounded-br-2xl";
-						}
-					})();
+					
+					// Border Radius
+					const rounded = roundedForImageAndVideo(index, post.medias.length);
+					const sizeIndex = resizeForImage(index, post.medias.length);
 
 					if (isImage(media))
 						return (
 							<div
-								className={`relative max-w-[200px] max-h-[200px] ${rounded} hover:brightness-75 overflow-hidden cursor-pointer transition-all duration-150`}
+								// className={`relative max-w-[200px] max-h-[200px] ${rounded} hover:brightness-75 overflow-hidden cursor-pointer transition-all duration-150`}
+								className={`relative ${sizeIndex} ${rounded} hover:brightness-75 overflow-hidden cursor-pointer transition-all duration-150 
+									${(index === 2 && post.medias.length === 3) ? "col-start-1 col-end-3" : ""}`}
 								key={index}
 								style={{
 									transform: "translate3d(0px, 0px, 0.1px)",
 								}}
-								onClick={() => console}
-							>
+								onClick={() => console}>
 								<img className={`w-full h-full object-cover ${rounded} ${post.medias.length > 4 && index == 3 && "brightness-50"} pointer-events-none`} src={media} />
 								
 								<span className="absolute z-50 text-[35px] text-white top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] pointer-events-none">
